ee646f3852545efbf2b9c81a6981110e99cc1320
Cong Minh <huynhcongminh04@gmail.com>
2024-12-21 15:08:32 +0700

    Initial commit: Story reading website with React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Components/Header/Header";
+import MainContent from "./Components/MainContent/MainContent";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <MainContent />
+    </div>
+  );
+};
+export default App;
